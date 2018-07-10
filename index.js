ab05f280b16f182cbc0ffa51be0332919946dd45
Sunny Renukuntla <sunnyr791@gmail.com>
2018-07-10 16:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+
+
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(name1) {
+  return 'Happy holidays, ${name1}!' 
+}
+
+function happyHolidayTo (holiday1, name2) {
+  return 'Happy '+ ${holiday1} + ',' + ${name2} + '!'
+}
+
+function holidayCountdown (holiday, days) {
+  return "It's " + ${days} +' days until ' + ${holiday} + '!'
+}
